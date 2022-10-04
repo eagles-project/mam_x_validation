0d43af4b3fb0731cdee3efbc0c683161d9a909c9
Jeffrey N. Johnson <jeff@cohere-llc.com>
2022-10-04 08:33:39 -0700

    Added placeholder scripts for validating nucleation results.

---
+++ nucleation/compare_particle_growth.py
@@ -0,0 +1,32 @@
+import os, sys, importlib
+
+# Look for data in whatever directory we're running in.
+sys.path.append(os.getcwd())
+
+def usage():
+    print('compare_particle_growth.py: compares particle cluster growth rates in Python data modules.')
+    print('usage: python3 compare_particle_growth.py <module1.py> <module2.py>')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        usage()
+        exit(0)
+
+    # Import the given data modules.
+    data1 = importlib.import_module(sys.argv[1].replace('.py', ''))
+    data2 = importlib.import_module(sys.argv[2].replace('.py', ''))
+
+    # Make sure that the input and output names are identical.
+    inputs1 = dir(data1.input)
+    inputs1.sort()
+    inputs2 = dir(data2.input)
+    inputs2.sort()
+    assert(inputs1 == inputs2)
+
+    outputs1 = dir(data1.output1)
+    outputs1.sort()
+    outputs2 = dir(data2.input)
+    outputs2.sort()
+    assert(outputs1 == outputs2)
+
+    # FIXME: quantitative comparisons go here!
