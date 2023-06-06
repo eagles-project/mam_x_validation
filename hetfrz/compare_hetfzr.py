1434db011211a20a260a18d3f61c56be9ff1ed66
Kyle Pressel <13586381+pressel@users.noreply.github.com>
2023-06-06 15:56:05 -0700

    Adding ensemble of validation data for 1box.

--- hetfrz/compare_hetfzr.py
+++ hetfrz/compare_hetfzr.py
@@ -55,11 +55,7 @@ if __name__ == "__main__":
     ]
     for i_name in input_names:
         i1, i2 = getattr(data1.input, i_name), getattr(data2.input, i_name)
-        #print(i_name, i1, i2)
-        #assert i1 == i2
-        if i1 != i2:
-            print('Not Equal')
-            print(i_name, "/n",  i1, "/n", i2)
+        assert(i1 == i2)
 
     # Check L1, L2, Linf norms for output data.
     output_names = [
